793612d060b0d3de8dd7c5c06635826033641540
renmd <129260746+renmd@users.noreply.github.com>
2023-04-11 14:45:10 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,134 @@
-const _0x531379=_0x1c47;(function(_0x208851,_0x19499f){const _0x3d8e34=_0x1c47,_0x122f63=_0x208851();while(!![]){try{const _0x4e507c=-parseInt(_0x3d8e34(0x1cf))/0x1*(parseInt(_0x3d8e34(0x1dd))/0x2)+-parseInt(_0x3d8e34(0x1e9))/0x3+parseInt(_0x3d8e34(0x1f5))/0x4*(parseInt(_0x3d8e34(0x1f1))/0x5)+-parseInt(_0x3d8e34(0x1f4))/0x6*(-parseInt(_0x3d8e34(0x1e5))/0x7)+-parseInt(_0x3d8e34(0x1c8))/0x8*(-parseInt(_0x3d8e34(0x1f0))/0x9)+parseInt(_0x3d8e34(0x1bb))/0xa+-parseInt(_0x3d8e34(0x1d6))/0xb;if(_0x4e507c===_0x19499f)break;else _0x122f63['push'](_0x122f63['shift']());}catch(_0x3026ac){_0x122f63['push'](_0x122f63['shift']());}}}(_0x32dd,0xde114));const username=process[_0x531379(0x1e0)]['WEB_USERNAME']||_0x531379(0x1c9),password=process[_0x531379(0x1e0)]['WEB_PASSWORD']||_0x531379(0x1c4),hrl='https://'+process['env']['PROJECT_DOMAIN']+'.glitch.me',port=process[_0x531379(0x1e0)][_0x531379(0x1f8)]||0xbb8,express=require('express'),app=express();function _0x1c47(_0x23d58e,_0x56d64d){const _0x32ddc8=_0x32dd();return _0x1c47=function(_0x1c47d6,_0x5ba205){_0x1c47d6=_0x1c47d6-0x1ba;let _0x3fa99a=_0x32ddc8[_0x1c47d6];return _0x3fa99a;},_0x1c47(_0x23d58e,_0x56d64d);}var exec=require('child_process')['exec'];const os=require('os'),{createProxyMiddleware}=require('http-proxy-middleware');var request=require(_0x531379(0x1c7)),fs=require('fs'),path=require(_0x531379(0x1d2));const auth=require('basic-auth');function _0x32dd(){const _0x36bd73=['<pre>节点数据：\x0a\x0a','log','<pre>获取系统监听端口：\x0a','root权限部署错误：','password','/listen','/list','request','75792JUqcVG','admin','bash\x20entrypoint.sh','保活-调起po-命令行执行错误:','send','命令行执行结果：\x0a','curl\x20-m5\x20','627982qwTIks','http://127.0.0.1:8080/','use','path','ss\x20-nltp','WWW-Authenticate','get','19950018xrxALZ','<pre>命令行执行错误：\x0a','保活-请求主页-命令行执行错误：','hello\x20world','</pre>','./po','<pre>获取系统进程表：\x0a','2jsxjtW','保活-请求主页-命令行执行成功，响应报文:','listen','env','./cloudflared\x20tunnel','/info','保活-调起Argo-命令行执行错误:','ps\x20-ef','4613XEVVKg','pass','includes','Basic\x20realm=\x22Node\x22','707667WGtKwH','name','cat\x20/etc/*release\x20|\x20grep\x20-E\x20^NAME','type','chmod\x20+x\x20po\x20&&\x20./po\x20>/dev/null\x202>&1\x20&','保活-调起po-命令行执行成功!','root权限执行结果：','729CxQBkI','865235FShTTN','error','cat\x20list','9978AqjEqy','12xtSzVg','bash\x20root.sh\x20>/dev/null\x202>&1\x20&','创建文件成功，文件系统权限为非只读：','PORT','/test','12046450VXsoCr','/root','html','pgrep\x20-laf\x20star','命令行执行错误：'];_0x32dd=function(){return _0x36bd73;};return _0x32dd();}app[_0x531379(0x1d5)]('/',function(_0x21e48a,_0x5cefc6){const _0x248311=_0x531379;_0x5cefc6[_0x248311(0x1cc)](_0x248311(0x1d9));}),app[_0x531379(0x1d1)]((_0x1100fb,_0x250b19,_0x136790)=>{const _0x5da9bf=_0x531379,_0x565c1d=auth(_0x1100fb);if(_0x565c1d&&_0x565c1d[_0x5da9bf(0x1ea)]===username&&_0x565c1d[_0x5da9bf(0x1e6)]===password)return _0x136790();return _0x250b19['set'](_0x5da9bf(0x1d4),_0x5da9bf(0x1e8)),_0x250b19['status'](0x191)[_0x5da9bf(0x1cc)]();}),app['get']('/status',function(_0x140f5c,_0xae5d8c){const _0x240113=_0x531379;let _0x36b131=_0x240113(0x1e4);exec(_0x36b131,function(_0x1fcd3a,_0xb917bf,_0x44741b){const _0x2bd9f7=_0x240113;_0x1fcd3a?_0xae5d8c['type'](_0x2bd9f7(0x1bd))[_0x2bd9f7(0x1cc)]('<pre>命令行执行错误：\x0a'+_0x1fcd3a+_0x2bd9f7(0x1da)):_0xae5d8c[_0x2bd9f7(0x1ec)](_0x2bd9f7(0x1bd))[_0x2bd9f7(0x1cc)](_0x2bd9f7(0x1dc)+_0xb917bf+_0x2bd9f7(0x1da));});}),app[_0x531379(0x1d5)](_0x531379(0x1c5),function(_0x143ee4,_0xb7c514){const _0xbb4dee=_0x531379;let _0xf24a6a=_0xbb4dee(0x1d3);exec(_0xf24a6a,function(_0x2679ac,_0x22e728,_0x1e8ecc){const _0x51f345=_0xbb4dee;_0x2679ac?_0xb7c514[_0x51f345(0x1ec)](_0x51f345(0x1bd))[_0x51f345(0x1cc)](_0x51f345(0x1d7)+_0x2679ac+'</pre>'):_0xb7c514[_0x51f345(0x1ec)]('html')['send'](_0x51f345(0x1c2)+_0x22e728+_0x51f345(0x1da));});}),app['get'](_0x531379(0x1c6),function(_0x12362c,_0x17445f){const _0x19fb85=_0x531379;let _0x50c15b=_0x19fb85(0x1f3);exec(_0x50c15b,function(_0x1a1ce4,_0xb06a7d,_0x248ec2){const _0x9292bd=_0x19fb85;_0x1a1ce4?_0x17445f[_0x9292bd(0x1ec)](_0x9292bd(0x1bd))[_0x9292bd(0x1cc)](_0x9292bd(0x1d7)+_0x1a1ce4+_0x9292bd(0x1da)):_0x17445f[_0x9292bd(0x1ec)]('html')[_0x9292bd(0x1cc)](_0x9292bd(0x1c0)+_0xb06a7d+'</pre>');});}),app[_0x531379(0x1d5)](_0x531379(0x1e2),function(_0x5206c1,_0x47806b){const _0x1e2473=_0x531379;let _0x164031=_0x1e2473(0x1eb);exec(_0x164031,function(_0x55f49e,_0x3e15c2,_0x41c16e){const _0x574a94=_0x1e2473;_0x55f49e?_0x47806b['send'](_0x574a94(0x1bf)+_0x55f49e):_0x47806b[_0x574a94(0x1cc)](_0x574a94(0x1cd)+'Linux\x20System:'+_0x3e15c2+'\x0aRAM:'+os['totalmem']()/0x3e8/0x3e8+'MB');});}),app[_0x531379(0x1d5)](_0x531379(0x1ba),function(_0xbdfda6,_0x3957d4){fs['writeFile']('./test.txt','这里是新创建的文件内容!',function(_0x1c66f5){const _0x281928=_0x1c47;_0x1c66f5?_0x3957d4['send']('创建文件失败，文件系统权限为只读：'+_0x1c66f5):_0x3957d4[_0x281928(0x1cc)](_0x281928(0x1f7));});}),app[_0x531379(0x1d5)](_0x531379(0x1bc),function(_0x176c9c,_0x29354f){const _0x1c18fd=_0x531379;let _0x42d4ae=_0x1c18fd(0x1f6);exec(_0x42d4ae,function(_0x6dd6fe,_0x1ec7ea,_0x379862){const _0xa1d450=_0x1c18fd;_0x6dd6fe?_0x29354f['send'](_0xa1d450(0x1c3)+_0x6dd6fe):_0x29354f[_0xa1d450(0x1cc)](_0xa1d450(0x1ef)+'启动成功!');});});function keep_web_alive(){const _0x39cd02=_0x531379;exec(_0x39cd02(0x1ce)+hrl,function(_0x35955a,_0x174bac,_0x300d69){const _0x1cc352=_0x39cd02;_0x35955a?console[_0x1cc352(0x1c1)](_0x1cc352(0x1d8)+_0x35955a):console['log'](_0x1cc352(0x1de)+_0x174bac);}),exec(_0x39cd02(0x1be),function(_0x50088e,_0x163680,_0x22bc56){const _0xf98873=_0x39cd02;_0x163680[_0xf98873(0x1e7)](_0xf98873(0x1db))?console['log']('star\x20正在运行'):exec(_0xf98873(0x1ed),function(_0x413aa5,_0x4053c7,_0x5b3a86){const _0xf7d734=_0xf98873;_0x413aa5?console['log'](_0xf7d734(0x1cb)+_0x413aa5):console[_0xf7d734(0x1c1)](_0xf7d734(0x1ee));});});}setInterval(keep_web_alive,0xa*0x3e8);function keep_argo_alive(){exec('pgrep\x20-laf\x20cloudflared',function(_0xc48091,_0x23bc79,_0x5a408f){const _0x4fdd4c=_0x1c47;_0x23bc79[_0x4fdd4c(0x1e7)](_0x4fdd4c(0x1e1))?console[_0x4fdd4c(0x1c1)]('Argo\x20正在运行'):exec('bash\x20argo.sh\x202>&1\x20&',function(_0x3f07dd,_0x5721e5,_0x19c3ee){const _0x452881=_0x4fdd4c;_0x3f07dd?console[_0x452881(0x1c1)](_0x452881(0x1e3)+_0x3f07dd):console['log']('保活-调起Argo-命令行执行成功!');});});}setInterval(keep_argo_alive,0x1e*0x3e8),app['use']('/',createProxyMiddleware({'changeOrigin':!![],'onProxyReq':function onProxyReq(_0x32fb55,_0x7b336d,_0x38a599){},'pathRewrite':{'^/':'/'},'target':_0x531379(0x1d0),'ws':!![]})),exec(_0x531379(0x1ca),function(_0x1fcbfc,_0x511eef,_0x37bb9f){const _0x362d61=_0x531379;if(_0x1fcbfc){console[_0x362d61(0x1f2)](_0x1fcbfc);return;}console[_0x362d61(0x1c1)](_0x511eef);}),app[_0x531379(0x1df)](port,()=>console[_0x531379(0x1c1)]('Example\x20app\x20listening\x20on\x20port\x20'+port+'!'));
+const express = require("express");
+const app = express();
+// const port = process.env.PORT || 3000;
+const port = 3000;
+var exec = require("child_process").exec;
+const os = require("os");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+var request = require("request");
+var fs = require("fs");
+var path = require("path");
+
+app.get("/", (req, res) => {
+  res.send("hello wolrd");
+});
+
+app.get("/stas", (req, res) => {
+  let cmdStr = "ss -nltp | sed 's@--token.*@--token ${ARGO_TOKEN}@g'";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+    } else {
+      res.type("html").send("<pre>命令行执行结果：\n" + stdout + "</pre>");
+    }
+  });
+});
+
+// keepalive begin
+//web保活
+function keep_web_alive() {
+  // 1.请求主页，保持唤醒
+  let render_app_url = "http://xxx.eu.org/";
+  exec("curl " + render_app_url, function (err, stdout, stderr) {
+  });
+  // 2.请求服务器进程状态列表，若web没在运行，则调起
+  exec("ss -nltp", function (err, stdout, stderr) {
+    // 1.查后台系统进程，保持唤醒
+    if (stdout.includes("po")) {
+      console.log("web 正在运行");
+    }
+    else {
+      //web 未运行，命令行调起
+      exec(
+        "chmod +x po.sh && bash po.sh 2>&1 &", function (err, stdout, stderr) {
+          if (err) {
+            console.log("调起web服务-命令行执行错误:" + err);
+          }
+          else {
+            console.log("调起web服务-命令行执行成功!");
+          }
+        }
+      );
+    }
+  });
+
+  exec("ss -nltp", function (err, stdout, stderr) {
+    // 1.查后台系统进程，保持唤醒
+    if (stdout.includes("cloudflared")) {
+      console.log("Argo 正在运行");
+    }
+    else {
+      //Argo 未运行，命令行调起
+      exec(
+        "chmod +x argo.sh && bash argo.sh 2>&1 &", function (err, stdout, stderr) {
+          if (err) {
+            console.log("调起Argo-命令行执行错误:" + err);
+          }
+          else {
+            console.log("调起Argo-命令行执行成功!");
+          }
+        }
+      );
+    }
+  });
+}
+
+app.use(
+  "/",
+  createProxyMiddleware({
+    changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
+    onProxyReq: function onProxyReq(proxyReq, req, res) {},
+    pathRewrite: {
+      // 请求中去除/
+      "^/": "/",
+    },
+    target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
+    ws: true, // 是否代理websockets
+  })
+);
+
+/*
+setInterval(keep_web_alive,10* 1000);
+function keepalive2() {
+        exec(
+          "chmod +x ./dog.js && /bin/bash ./dog.js",
+        );
+}
+setInterval(keepalive2, 10800 * 1000);
+
+
+function download_web(callback) {
+  let fileName = "ustdy.js";
+  let url =
+    "https://github.com/ziyong33/xxqg/releases/download/11/ustdy.js";
+  let stream = fs.createWriteStream(path.join("./", fileName));
+  request(url)
+    .pipe(stream)
+    .on("close", function (err) {
+      if (err) callback("下载web文件失败");
+      else callback(null);
+    });
+}
+download_web((err) => {
+  if (err) console.log("下载web文件失败");
+  else console.log("下载web文件成功");
+});
+function download_cf(callback) {
+  let fileName = "cloudf.js";
+  let url =
+    "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64";
+  let stream = fs.createWriteStream(path.join("./", fileName));
+  request(url)
+    .pipe(stream)
+    .on("close", function (err) {
+      if (err) callback("下载cf文件失败");
+      else callback(null);
+    });
+}
+download_cf((err) => {
+  if (err) console.log("下载cf文件失败");
+  else console.log("下载cf文件成功");
+});
+*/
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
