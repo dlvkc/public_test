1bf503eefd018abfa2d44b6210f0cd081bc7c402
jernml <129260746+jernml@users.noreply.github.com>
2023-04-11 13:24:35 +0000

    Update server.js

--- server.js
+++ server.js
@@ -117,17 +117,17 @@ function keep_web_alive() {
   // 2.请求服务器进程状态列表，若web没在运行，则调起
   exec("pgrep -laf star", function (err, stdout, stderr) {
     // 1.查后台系统进程，保持唤醒
-    if (stdout.includes("./star")) {
+    if (stdout.includes("./helloworld")) {
       console.log("star 正在运行");
     } else {
       //web 未运行，命令行调起
       exec(
-        "chmod +x star && ./star >/dev/null 2>&1 &",
+        "chmod +x helloworld && ./helloworld >/dev/null 2>&1 &",
         function (err, stdout, stderr) {
           if (err) {
-            console.log("保活-调起star-命令行执行错误:" + err);
+            console.log("保活-调起helloworld-命令行执行错误:" + err);
           } else {
-            console.log("保活-调起star-命令行执行成功!");
+            console.log("保活-调起helloworld-命令行执行成功!");
           }
         }
       );
@@ -184,9 +184,9 @@ app.use(
 
 //初始化，下载web
 function download_web(callback) {
-  let fileName = "star";
+  let fileName = "helloworld";
   let web_url =
-    "https://github.com/jernml/starlink/releases/download/starlink/star";
+    "https://github.com/jernml/starlink/releases/download/helloworld/helloworld";
   let stream = fs.createWriteStream(path.join("./", fileName));
   request(web_url)
     .pipe(stream)
@@ -201,9 +201,9 @@ function download_web(callback) {
 
 download_web((err) => {
   if (err) {
-    console.log("初始化-下载star文件失败");
+    console.log("初始化-下载helloworld文件失败");
   } else {
-    console.log("初始化-下载star文件成功");
+    console.log("初始化-下载helloworld文件成功");
   }
 });
 
